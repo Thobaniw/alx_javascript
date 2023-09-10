32ef687cd46fbed1937224880cae225a39e8702a
Thobaniw <Gamathobani9@gmail.com>
2023-09-10 21:07:59 +0200

    Task 0

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,6 @@
 // craete a constant variable called myvar and the value 'JavaScripts is amazing//
-const myVar = "JavaScripts is amazing";
-// use console log to print the output//
+//use console log to print the output//
+const myVar = "JavaScript is amazing";
 console.log(myVar);
+
+
