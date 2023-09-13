fe46b2ceeb31bcc13699d248bf663a7d0ff8e343
Thobaniw <Gamathobani9@gmail.com>
2023-09-13 22:24:08 +0200

    Task 4

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,7 +1,6 @@
 // write a function that returns addition two intergers //
-function add(a,b) {
-    return a + b ;
+function add(a, b) {
+    return a + b;
 }
-//create a const variable called sum that will contain two numbers make it  equal to the function
-const sum = add(4,85);
-console.log(sum);
+
+console.log(add(4, 85));
