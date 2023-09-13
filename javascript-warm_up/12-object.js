e4d270124587e94b987f24927c68c4a9b5e19ed6
Thobaniw <Gamathobani9@gmail.com>
2023-09-13 22:01:16 +0200

    Task 3

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/Node
+const myObject = {
+    type : 'object',
+    value : 12
+};
+console.log(myObject);
+
+//replace the value 12 with 89 using myObject.value//
+myObject.value = 89;
+console.log(myObject);
