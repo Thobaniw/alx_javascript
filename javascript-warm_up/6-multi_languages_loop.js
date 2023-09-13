1b883f3c0dbb1b5703f89beeffc245a170bc171e
Thobaniw <Gamathobani9@gmail.com>
2023-09-13 21:35:08 +0200

    Task 2

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+// Write a script that print 3 lines and must use while and for loops//
+// First create an array or a line of the 3 lines you want to print//
+const lines = ["C is fun","Python is cool","JavaScript is amazing"];
+// create a for loop to iterate through the array, and in each iteration//
+// we use console.log to print the current string from the array//
+for (let i = 0; i< lines.length; i++) {
+    console.log(lines[i]);
+}
