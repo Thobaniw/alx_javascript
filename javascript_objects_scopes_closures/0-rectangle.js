26ccbc26b5d4d51bae9b011b9e162a64f8f8ffe6
Thobaniw <Gamathobani9@gmail.com>
2023-09-14 22:24:04 +0200

    Task 0

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,10 +1,9 @@
 class Rectangle {
-    constructor() {
-      // Constructor can be empty or include initialization logic
-    }
+    // Constructor can be empty or include initialization logic
   }
   
   // Create an instance of Rectangle
   const r1 = new Rectangle();
   console.log(r1); // This should print: Rectangle {}
+  
   
