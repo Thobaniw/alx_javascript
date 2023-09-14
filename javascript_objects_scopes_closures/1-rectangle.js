ca8a6c125f364a9d818383585fc41212b2f2e4b1
Thobaniw <Gamathobani9@gmail.com>
2023-09-14 23:01:32 +0200

    Task 1

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+// Create a class rectangle//
+class Rectangle {
+    constructor(w,h){
+        this.width = w
+        this.height = h
+    }
+}
+
+const r1 = new Rectangle(2,3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
