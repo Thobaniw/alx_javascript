f8066201360429e3757af9b5839571f729a7f34c
Thobaniw <Gamathobani9@gmail.com>
2023-09-18 22:21:13 +0200

    Task1

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+Script that prints the title of a Star Wars movie where the episode
+number matches a given integer.
+*/
+
+const args = process.argv;
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + args[2], function (err, request) {
+  if (err) throw err;
+  const data = JSON.parse(request.body);
+  console.log(data.title);
+});
