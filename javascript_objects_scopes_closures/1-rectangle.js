a5effbef8dd58471237aeabe63b24bb3d4d0b8d7
Thobaniw <Gamathobani9@gmail.com>
2023-09-18 03:35:42 +0200

    Task 1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,32 +1,32 @@
-// Create a class rectangle//
 class Rectangle {
-    constructor(w,h){
-        this.width = w
-        this.height = h
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
     }
-}
-
-const r1 = new Rectangle(3,3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r2 = new Rectangle(3,-3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
-
-const r3 = new Rectangle(3);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
-
-const r4 = new Rectangle(undefined,3);
-console.log(r4);
-console.log(r4.width);
-console.log(r4.height);
-
-const r5 = new Rectangle();
-console.log(r5);
-console.log(r5.width);
-console.log(r5.height);
+  }
+  
+  const r1 = new Rectangle(3, 3);
+  console.log(r1); // Output: Rectangle { width: 3, height: 3 }
+  console.log(r1.width); // Output: 3
+  console.log(r1.height); // Output: 3
+  
+  const r2 = new Rectangle(3, -3);
+  console.log(r2); // Output: Rectangle { width: 3, height: -3 }
+  console.log(r2.width); // Output: 3
+  console.log(r2.height); // Output: -3
+  
+  const r3 = new Rectangle(3);
+  console.log(r3); // Output: Rectangle { width: 3, height: undefined }
+  console.log(r3.width); // Output: 3
+  console.log(r3.height); // Output: undefined
+  
+  const r4 = new Rectangle(undefined, 3);
+  console.log(r4); // Output: Rectangle { width: undefined, height: 3 }
+  console.log(r4.width); // Output: undefined
+  console.log(r4.height); // Output: 3
+  
+  const r5 = new Rectangle();
+  console.log(r5); // Output: Rectangle { width: undefined, height: undefined }
+  console.log(r5.width); // Output: undefined
+  console.log(r5.height); // Output: undefined
+  
