04490dfe735aa7fd3640656e02f346c548ac182e
Thobaniw <Gamathobani9@gmail.com>
2023-09-18 04:30:49 +0200

    Task 1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,35 +1,8 @@
-class Rectangle {
-    constructor(width, height) {
-      this.width = width;
-      this.height = height;
+#!/usr/bin/node
+module.exports = class Rectangle {
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
     }
-  }
-  module.export = Rectangle; // export a class
-  const Rectangle = require('./rectangle.js'); // Import the class
-
-
-  const r1 = new Rectangle(3, 3);
-  console.log(r1); // Output: Rectangle { width: 3, height: 3 }
-  console.log(r1.width); // Output: 3
-  console.log(r1.height); // Output: 3
-  
-  const r2 = new Rectangle(3, -3);
-  console.log(r2); // Output: Rectangle { width: 3, height: -3 }
-  console.log(r2.width); // Output: 3
-  console.log(r2.height); // Output: -3
-  
-  const r3 = new Rectangle(3);
-  console.log(r3); // Output: Rectangle { width: 3, height: undefined }
-  console.log(r3.width); // Output: 3
-  console.log(r3.height); // Output: undefined
-  
-  const r4 = new Rectangle(undefined, 3);
-  console.log(r4); // Output: Rectangle { width: undefined, height: 3 }
-  console.log(r4.width); // Output: undefined
-  console.log(r4.height); // Output: 3
-  
-  const r5 = new Rectangle();
-  console.log(r5); // Output: Rectangle { width: undefined, height: undefined }
-  console.log(r5.width); // Output: undefined
-  console.log(r5.height); // Output: undefined
+  };
   
